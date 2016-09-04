b318401148400052aad97fdf2d6e2a8093c6a97e
kirimaks <kirimaks@yahoo.com>
2016-09-04 14:58:02 +0300

    get_text() separator

--- my_scraper/tools/JujuTools.py
+++ my_scraper/tools/JujuTools.py
@@ -51,174 +51,296 @@ class Parser:
         return self.parsers[netloc]
 
     def juju_com(self, soup):
-        desc = soup.find_all("dd", attrs={'class': 'description'})[0]
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find_all("dd", attrs={'class': 'description'})[0]
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def themuse_com(self, soup):
-        desc = soup.find_all("div", attrs={'class': 'job-post-description'})[0]
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find_all("div", attrs={'class': 'job-post-description'})[0]
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def ihiresecondaryteachers_com(self, soup):
-        desc = soup.find_all("div", attrs={'class': 'job-description'})[0]
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find_all("div", attrs={'class': 'job-description'})[0]
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def startwire_com(self, soup):
-        desc = soup.find_all("div", attrs={'class': 'ua-job-description'})[0]
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find_all("div", attrs={'class': 'ua-job-description'})[0]
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def internationaleducationgroup_applytojob_com(self, soup):
-        desc = soup.find_all("div", attrs={"id": "resumator-job-description"})[0]
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find_all("div", attrs={"id": "resumator-job-description"})[0]
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def buffaloschools_applicantstack_com(self, soup):
-        # list(soup.find("div", id="ascontainer").div.next_siblings)[1].get_text(strip=True)
-        desc = soup.find("div", id="ascontainer").div.next_siblings
-        desc = list(desc)[1].get_text(strip=True)
+        try:
+            desc = soup.find("div", id="ascontainer").div.next_siblings
+            desc = list(desc)[1].get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def recruit_zohopublic_com(self, soup):
-        # desc = soup.find("div", attrs={"class": "job-dec-block_new"})
-        # desc = desc.get_text(strip=True)
         return "js site"
 
     def glassdoor_com(self, soup):
         return "js site"
 
     def anyonehiring_com(self, soup):
-        # list(soup.find("div", attrs={"class": "job-leftcontent"}).next_siblings)[1].get_text(strip=True)
-        desc = soup.find("div", attrs={"class": "job-leftcontent"}).next_siblings
-        desc = list(desc)[1].get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "job-leftcontent"}).next_siblings
+            desc = list(desc)[1].get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def careers_novastarprep_com(self, soup):
-        desc = soup.find("div", attrs={"class": "detailsJobDescription"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "detailsJobDescription"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def jobs_houstonisd_org(self, soup):
-        desc = soup.find("div", attrs={"class", "jobDisplay"})
-        # Remove js
-        [i.extract() for i in desc.find_all("script")]
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class", "jobDisplay"})
+            # Remove js
+            [i.extract() for i in desc.find_all("script")]
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def xcelhr_catsone_com(self, soup):
-        desc = soup.find("tr", id='trpositionsummary')
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("tr", id='trpositionsummary')
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def apply_embassysummer_com(self, soup):
-        desc = soup.find("div", attrs={"class": "description"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "description"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def takelessons_com(self, soup):
-        desc = soup.find("article", id="Listing")
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("article", id="Listing")
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def hirebridge_com(self, soup):
-        desc = soup.find("div", attrs={"class": "main"}).article
-        [i.extract() for i in desc.find_all("script")]
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "main"}).article
+            [i.extract() for i in desc.find_all("script")]
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
 
         return desc
 
     def englishfirst_com(self, soup):
-        desc = soup.find("div", attrs={"class", "describe"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class", "describe"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def careerbuilder_com(self, soup):
-        desc = soup.find("div", attrs={"class", "description"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class", "description"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def jobs_mpls_k12_mn_us(self, soup):
-        desc = soup.find("div", attrs={"class", "jobDisplay"})
-        [i.extract() for i in desc.find_all("script")]
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class", "jobDisplay"})
+            [i.extract() for i in desc.find_all("script")]
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def minnesotajobs_com(self, soup):
-        desc = soup.find("div", attrs={"class": "view_long"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "view_long"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def vipkid_us(self, soup):
-        desc = soup.find("div", attrs={"class": "job-posting"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "job-posting"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def main_hercjobs_org(self, soup):
-        desc = soup.find("div", attrs={"class": "bti-jd-description"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "bti-jd-description"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def jobs_washingtonpost_com(self, soup):
-        desc = soup.find("div", attrs={"itemprop": "description"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"itemprop": "description"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def www_prodivnet_com(self, soup):
-        desc = soup.find("div", id="description")
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", id="description")
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def sais_associationcareernetwork_com(self, soup):
-        desc = soup.find("span", id="lblJobDesc")
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("span", id="lblJobDesc")
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def legalforce_applytojob_com(self, soup):
-        desc = soup.find("div", id="resumator-job-description")
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", id="resumator-job-description")
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def educators_on_call_workable_com(self, soup):
-        desc = soup.find_all("section", attrs={"class": "section section--text"})[0]
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find_all("section", attrs={"class": "section section--text"})[0]
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def switchrecruit_catsone_com(self, soup):
-        desc = soup.find("div", attrs={"class": "detailsJobDescription"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "detailsJobDescription"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def www_JobsInME_com(self, soup):
-        desc = soup.find("p", id="docs-internal-guid-c9adc6d3-8e76-4327-a73d-44b8e48e759a")
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("p", id="docs-internal-guid-c9adc6d3-8e76-4327-a73d-44b8e48e759a")
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def studygroup_applytojob_com(self, soup):
-        desc = soup.find("div", attrs={"class": "description"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "description"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def www_disabledperson_com(self, soup):
-        desc = soup.find("div", attrs={"class": "job"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "job"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def www_vscyberhosting3_com(self, soup):
-        desc = soup.find("table", id="CRCareers1_tblJobDescrDetail")
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("table", id="CRCareers1_tblJobDescrDetail")
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
 
     def www_ivyexec_com(self, soup):
-        desc = soup.find("div", attrs={"class": "main-content"})
-        desc = desc.find_all("div", attrs={"class": "row-fluid"})[3]
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"class": "main-content"})
+            desc = desc.find_all("div", attrs={"class": "row-fluid"})[3]
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
+        return desc
 
     def careers_insidehighered_com(self, soup):
-        desc = soup.find("div", attrs={"itemprop": "description"})
-        desc = desc.get_text(strip=True)
+        try:
+            desc = soup.find("div", attrs={"itemprop": "description"})
+            desc = desc.get_text(" ", strip=True)
+        except IndexError:
+            desc = "null"
+
         return desc
+
+
 def get_juju_description(response):
     body = response.xpath("//body").extract()[0]
     soup = bs4.BeautifulSoup(body, "lxml")
@@ -226,23 +348,32 @@ def get_juju_description(response):
     try:
         parser = Parser().get_parser(netloc)
     except KeyError:
-        raise scrapy.exceptions.CloseSpider("No parser for: [{}], netloc: [{}]".format(response.url, netloc))
+        raise scrapy.exceptions.CloseSpider("No parser for: [{}],\
+ netloc: [{}]".format(response.url, netloc))
 
     return parser(soup)
 
 broken_links = [
     'https://www.workable.com/',
 
-    'http://www.prodivnet.com/job-summaries?job=5799e554e796554880000348&organization=Chicago+Public+Schools',
-    'http://www.prodivnet.com/job-summaries?job=57a07b6ee79655a32100156c&organization=Chicago+Public+Schools',
-    'http://www.prodivnet.com/job-summaries?job=57934e59e79655741200133b&organization=Chicago+Public+Schools',
-    'http://www.prodivnet.com/job-summaries?job=5799e624e79655488000038c&organization=Chicago+Public+Schools',
-    'http://www.prodivnet.com/job-summaries?job=57a0760fe79655b30a0001ef&organization%20=Seattle+Public+Schools',
-    'http://www.prodivnet.com/job-summaries?job=57a0760fe79655b30a0001ef&organization=Seattle+Public+Schools',
+    'http://www.prodivnet.com/job-summaries?\
+job=5799e554e796554880000348&organization=Chicago+Public+Schools',
+    'http://www.prodivnet.com/job-summaries?\
+job=57a07b6ee79655a32100156c&organization=Chicago+Public+Schools',
+    'http://www.prodivnet.com/job-summaries?\
+job=57934e59e79655741200133b&organization=Chicago+Public+Schools',
+    'http://www.prodivnet.com/job-summaries?\
+job=5799e624e79655488000038c&organization=Chicago+Public+Schools',
+    'http://www.prodivnet.com/job-summaries?\
+job=57a0760fe79655b30a0001ef&organization%20=Seattle+Public+Schools',
+    'http://www.prodivnet.com/job-summaries?\
+job=57a0760fe79655b30a0001ef&organization=Seattle+Public+Schools',
 
     'http://jobsearch.educationamerica.net/index.phtml?a=v&',
     'http://findjobinfo.com',
 
-    'https://simplyjobs.com/applicants/register/sign-up?feed=DirectEmployers&job_id=iCIMS14507-5975',
-    'https://simplyjobs.com/applicants/register/sign-up?feed=DirectEmployers&job_id=iCIMS14507-5877'
+    'https://simplyjobs.com/applicants/register/sign-up?\
+feed=DirectEmployers&job_id=iCIMS14507-5975',
+    'https://simplyjobs.com/applicants/register/sign-up?\
+feed=DirectEmployers&job_id=iCIMS14507-5877'
 ]
